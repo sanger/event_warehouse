713dd1ffcba145d2dbc8e2f1e0f93095ec446292
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-02-04 17:35:43 +0000

    Block the process for integration test

--- bin/run.sh
+++ bin/run.sh
@@ -4,7 +4,7 @@ if test "${INTEGRATION_TEST_SETUP}" = "true" ; then
   echo "Setting up for integration tests"
   RAILS_ENV=test bundle exec rake db:reset
   RAILS_ENV=test bundle exec rails runner /code/spec/data/integration/seed_for_unified_wh.rb
-  sleep 5
+  tail -f /dev/null
 fi
 
 ./bin/amqp_client start
