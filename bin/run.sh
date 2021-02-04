eb70adf714fbe4f94665ec08256e4d3a4bd89555
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-02-04 17:52:31 +0000

    Try similar to mysql

--- bin/run.sh
+++ bin/run.sh
@@ -1,10 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+set -eo pipefail
+shopt -s nullglob
 
-if test "${INTEGRATION_TEST_SETUP}" = "true" ; then
-  echo "Setting up for integration tests"
-  RAILS_ENV=test bundle exec rake db:reset
-  RAILS_ENV=test bundle exec rails runner /code/spec/data/integration/seed_for_unified_wh.rb
-  tail -f /dev/null
+
+# check to see if this file is being run or sourced from another script
+_is_sourced() {
+	# https://unix.stackexchange.com/a/215279
+	[ "${#FUNCNAME[@]}" -ge 2 ] \
+		&& [ "${FUNCNAME[0]}" = '_is_sourced' ] \
+		&& [ "${FUNCNAME[1]}" = 'source' ]
+}
+
+_main() {
+  if test "${INTEGRATION_TEST_SETUP}" = "true" ; then
+    echo "Setting up for integration tests"
+    RAILS_ENV=test bundle exec rake db:reset
+    RAILS_ENV=test bundle exec rails runner /code/spec/data/integration/seed_for_unified_wh.rb
+    tail -f /dev/null
+  fi
+  exec "$@"
+}
+
+# If we are sourced from elsewhere, don't perform any further actions
+if ! _is_sourced; then
+	_main "$@"
 fi
-
-./bin/amqp_client start
