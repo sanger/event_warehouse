3293d0b16326b155b07e1b6ee0a37d25190392e1
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-02-04 17:23:17 +0000

    Process start

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-if test -z "${INTEGRATION_TEST_SETUP}" ; then
-  ./bin/amqp_client start
-else
+
+if test "${INTEGRATION_TEST_SETUP}" = "true" ; then
   echo "Setting up for integration tests"
   RAILS_ENV=test bundle exec rake db:reset
+  RAILS_ENV=test bundle exec rails runner /code/spec/data/integration/seed_for_unified_wh.rb
+  sleep 5
 fi
+
+./bin/amqp_client start
