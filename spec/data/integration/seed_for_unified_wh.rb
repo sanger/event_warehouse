f0e3f1d0e41f1f28d4c5aed90f6b12cebaceeb12
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-08-11 16:23:41 +0100

    Add biosero events

--- spec/data/integration/seed_for_unified_wh.rb
+++ spec/data/integration/seed_for_unified_wh.rb
@@ -1,11 +1,3 @@
-# frozen_string_literal: true
-
-#
-# This file contains the seed data for running the integration tests from
-# the unified warehouse. Please do not remove it unless you modify the
-# corresponding code in the unified warehouse project, as there is a
-# dependency on the tests run in there.
-#
 BECKMAN_RECORD = {
   "event": {
     "uuid": '00000000-1111-2222-3333-111111111111',
@@ -80,7 +72,93 @@ OTHER_RECORD = {
   "lims": 'example'
 }.freeze
 
-EVENTS = [BECKMAN_RECORD, CHERRYPICK_RECORD, OTHER_RECORD].freeze
+
+BIOSERO_PLATE_COMPLETED_RECORD = {
+  "event": {
+    "uuid": '00000000-1111-2222-4444-111111111111',
+    "event_type": 'lh_biosero_cp_destination_plate_completed',
+    "occured_at": '2020-03-11 10:22:42',
+    "user_identifier": 'postmaster@example.com',
+    "subjects": [
+      {
+        "role_type": 'source',
+        "subject_type": 'sample',
+        "friendly_name": 'MY_SANGER_SAMPLE_ID_3',
+        "uuid": '00000000-1111-2222-4444-222222222222'
+      }
+    ],
+    "metadata": {
+    }
+  },
+  "lims": 'example'
+}.freeze
+
+
+### WE ARE HERE
+BIOSERO_PARTIAL_PLATE_COMPLETED_RECORD = {
+  "event": {
+    "uuid": '00000000-1111-2222-4444-333333333333',
+    "event_type": 'lh_biosero_cp_destination_plate_partial_completed',
+    "occured_at": '2020-03-11 10:22:42',
+    "user_identifier": 'postmaster@example.com',
+    "subjects": [
+      {
+        "role_type": 'source',
+        "subject_type": 'sample',
+        "friendly_name": 'MY_SANGER_SAMPLE_ID_4',
+        "uuid": '00000000-1111-2222-4444-444444444444'
+      }
+    ],
+    "metadata": {
+    }
+  },
+  "lims": 'example'
+}.freeze
+
+BIOSERO_ERROR_RECOVERED_PLATE_COMPLETED_RECORD = {
+  "event": {
+    "uuid": '00000000-1111-2222-4444-555555555555',
+    "event_type": 'lh_biosero_cp_error_recovered_destination_plate_completed',
+    "occured_at": '2020-03-11 10:22:42',
+    "user_identifier": 'postmaster@example.com',
+    "subjects": [
+      {
+        "role_type": 'source',
+        "subject_type": 'sample',
+        "friendly_name": 'MY_SANGER_SAMPLE_ID_5',
+        "uuid": '00000000-1111-2222-4444-666666666666'
+      }
+    ],
+    "metadata": {
+    }
+  },
+  "lims": 'example'
+}.freeze
+
+BIOSERO_ERROR_RECOVERED_PARTIAL_PLATE_COMPLETED_RECORD = {
+  "event": {
+    "uuid": '00000000-1111-2222-4444-777777777777',
+    "event_type": 'lh_biosero_cp_error_recovered_destination_plate_partial_completed',
+    "occured_at": '2020-03-11 10:22:42',
+    "user_identifier": 'postmaster@example.com',
+    "subjects": [
+      {
+        "role_type": 'source',
+        "subject_type": 'sample',
+        "friendly_name": 'MY_SANGER_SAMPLE_ID_6',
+        "uuid": '00000000-1111-2222-4444-888888888888'
+      }
+    ],
+    "metadata": {
+    }
+  },
+  "lims": 'example'
+}.freeze
+
+
+EVENTS = [BECKMAN_RECORD, CHERRYPICK_RECORD, OTHER_RECORD, BIOSERO_PLATE_COMPLETED_RECORD, 
+  BIOSERO_PARTIAL_PLATE_COMPLETED_RECORD, BIOSERO_ERROR_RECOVERED_PLATE_COMPLETED_RECORD, 
+  BIOSERO_ERROR_RECOVERED_PARTIAL_PLATE_COMPLETED_RECORD].freeze
 
 EVENTS.each do |data|
   Event.create_or_update_from_json(data[:event], data[:lims])
